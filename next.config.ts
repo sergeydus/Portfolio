6db078dec45ced59de4d12ec931274653faa7f35
sergeykimaia <gogosergey@gmail.com>
2025-11-06 21:06:29 +0200

    Configure GitHub Pages deployment
    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio',
 };
 
 export default nextConfig;
